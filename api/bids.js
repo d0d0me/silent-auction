f402c3d34e125746a8e7250bee18996cb2a10560
d0d0me <jerome@limpatience.com>
2025-04-22 21:24:50 -0400

    Update bids.js

--- api/bids.js
+++ api/bids.js
@@ -6,7 +6,7 @@ const supabase = createClient(
 );
 
 module.exports = async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://limpatience.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
